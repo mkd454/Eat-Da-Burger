6dcd25b499286823162a525a7dbd4fe55cb6ba3e
Mary Dang <dangmaryk@gmail.com>
2019-04-12 17:47:51 -0500

    fix 5 heroku connection

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
+  host: "localhost",
+  port: "3306",
   user: "root",
   password: "1245690",
   database: "burgers_db"
